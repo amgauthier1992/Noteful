73557444ce7a63c77060a02414c24bb23e5779e4
Andy Gauthier <amgauthier1992@gmail.com>
2020-09-30 10:33:32 -0400

    created error boundary

--- src/FolderPage.js
+++ src/FolderPage.js
@@ -8,7 +8,20 @@ class FolderPage extends React.Component {
   static contextType = Context;
   render() {
     const { folders, notes } = this.context;
-    // console.log(props.match.params.folderId);
+
+    //returns an object if the folder id matches the current route prop value
+    //inside the params object. If there are no matches, it will return
+    //undefined. We then validate if the folder object we create exists.
+    const folder = folders.find((folder) => {
+      if (folder.id === this.props.match.params.folderId) {
+        console.log(folder);
+        return folder;
+      }
+    });
+    if (folder === undefined) {
+      throw new Error("This folder doesn't exist");
+    }
+    // console.log(this.props.match.params.folderId);
     const folderNotes = notes.filter((note) => {
       if (note.folderId === this.props.match.params.folderId) {
         return note;
